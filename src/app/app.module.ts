b6ae93242a1ee08f54e5fbfda9604b5cd7a4baea
Felipe Soller <soller@outlook.com>
2021-04-09 13:35:01 -0300

    feat: Adiciona módulo httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -18,6 +19,7 @@ import { EffectsModule } from '@ngrx/effects';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     HomeModule,
     BookmarksModule,
     StoreModule.forRoot({}),
