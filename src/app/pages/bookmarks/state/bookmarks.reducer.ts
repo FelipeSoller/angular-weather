fd1f2a24afdf29a74b020fbae179a1306b84bb2c
Felipe Soller <soller@outlook.com>
2021-04-12 19:35:27 -0300

    feat: Desenvolve lógica das actions, effects, reducer e selectors da página de favoritos

--- src/app/pages/bookmarks/state/bookmarks.reducer.ts
+++ src/app/pages/bookmarks/state/bookmarks.reducer.ts
@@ -2,7 +2,7 @@ import { createReducer, Action, on } from '@ngrx/store';
 
 import { Bookmark } from 'src/app/shared/models/bookmark.model';
 import * as fromHomeActions from '../../home/state/home.actions';
-import * as fromBookmarksAction from './bookmarks.actions';
+import * as fromBookmarksActions from './bookmarks.actions';
 
 export interface BookmarksState {
   list: Bookmark[];
@@ -18,9 +18,13 @@ const reducer = createReducer(
     ...state,
     list: toggleBookmark(state.list, entity),
   })),
-  on(fromBookmarksAction.removeBookmark, (state, { id }) => ({
+  on(fromBookmarksActions.removeBookmark, (state, { id }) => ({
     ...state,
     list: state.list.filter(b => b.id !== id),
+  })),
+  on(fromBookmarksActions.updateBookmarksList, (state, { list }) => ({
+    ...state,
+    list,
   })),
 );
 
@@ -32,8 +36,5 @@ function toggleBookmark(list: Bookmark[], entity: Bookmark): Bookmark[] {
   if (!!list.find(bookmark => bookmark.id === entity.id)) {
     return list.filter(bookmark => bookmark.id !== entity.id);
   }
-  return [
-    ...list,
-    entity
-  ];
+  return [...list, entity];
 }
